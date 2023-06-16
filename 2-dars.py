dfb117cd7560142f4ae31acceb63f888f3e14741
Javohir <javohir200211@gmail.com>
2023-06-16 16:40:01 +0500

    lesson 2 completed

--- 2-dars.py
+++ 2-dars.py
@@ -1,41 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x_soat = np.array([1.0, 2.0, 3.0])
-y_baho = np.array([2.0, 4.0, 6.0 ])
+# data
+x_soat = np.array([1, 2, 3])
+y_baho = np.array([2, 4, 6])
 
-def forward(x):
+def forward(x, w):
     """To'g'riga hisoblash funksiyasi"""
     return x * w
 
-def loss(x, y):
-    """Xatolik (Loss) ning funksiyasi"""
-    y_pred = forward(x)
-    return (y_pred - y)**2
+def loss(y_true, y_pred):
+    """Xatolik (loss)ni hisoblash funksiyasi"""
+    return (y_pred - y_true)**2
 
-# Grafikni yaratib olishimiz uchun konteynerlar
-w_list = []
+# Grafik uchun Weight va MSE qiymatlari
+weights = []
 mse_list = []
-
-# w ni 0 dan 4 gacha oraliqda hisblash 
 for w in np.arange(0.0, 4.1, 0.1):
-    print(f"w = {w}")
-    L_umum=0
-    
-    for x_hb_qiym, y_hb_qiym in zip(x_soat, y_baho):
-        y_hb_bash = forward(x_hb_qiym)
-        L_hb_qiym = loss(x_hb_qiym, y_hb_qiym)
-        L_umum += L_hb_qiym
-        print("\t", "{:.2f}, {:.2f}, {:.2f}, {:.2f}".format(x_hb_qiym, y_hb_qiym, y_hb_bash, L_hb_qiym))
-    
-    # Har bir ma'lumot uchun MSE ni hisoblaymiz
-    print(f"MSE = {L_umum / len(x_soat)}")  # len(x_soat) -> N
-    w_list.append(w)
-    mse_list.append(L_umum / len(x_soat))
+    print(f'Weight={w:.2f}')
+    print(f'\tSoat\tBaho\tBashorat\tXatolik')
+    errors = []
 
-# Grafik natija
-plt.plot(w_list, mse_list, linewidth=4)
+    for x, y in zip(x_soat, y_baho):
+        y_pred = forward(x, w)
+        error = loss(y, y_pred)
+        print(f'\t{x}\t{y}\t{y_pred:.3f}\t\t{error:.3f}')
+        errors.append(error)
+    
+    mse = sum(errors) / len(errors)  # mean squared error
+    print(f'MSE={mse}\n')
+    weights.append(w)
+    mse_list.append(mse)
+
+# Weight va MSE qiymatlari grafikda
+plt.plot(weights, mse_list, linewidth=4)
 plt.ylabel('Loss', fontsize=20)
-plt.xlabel('W', fontsize=20)
+plt.xlabel('Weight', fontsize=20)
 plt.grid()
 plt.show()
